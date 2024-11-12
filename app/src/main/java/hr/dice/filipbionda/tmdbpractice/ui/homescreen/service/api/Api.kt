c1186227c548678ddb7c2c449f39433928362ea0
filipjosipbionda <filip.josip.bionda@gmail.com>
2024-11-12 22:47:49 +0100

    Removed unused code

--- app/src/main/java/hr/dice/filipbionda/tmdbpractice/ui/homescreen/service/api/Api.kt
+++ app/src/main/java/hr/dice/filipbionda/tmdbpractice/ui/homescreen/service/api/Api.kt
@@ -2,5 +2,4 @@ package hr.dice.filipbionda.tmdbpractice.ui.homescreen.service.api
 
 object Api {
      const val KEY = "ba4ef3ff802f84dba80dd15aa49ae812"
-     const val REFRESH_KEY = ""
 }
--- app/src/main/java/hr/dice/filipbionda/tmdbpractice/ui/homescreen/service/api/ApiRoutes.kt
+++
@@ -1,5 +0,0 @@
-package hr.dice.filipbionda.tmdbpractice.ui.homescreen.service.api
-
-object ApiRoutes {
-    val BASE_URL = "https://api.themoviedb.org/3"
-}
